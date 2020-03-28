934d83890765c75f9ed18f6ea2c10cc5b9dad684
Ika <ikatyang@gmail.com>
2020-03-28 14:39:22 +0800

    chore: sort and remove unused apps

--- install.sh
+++ install.sh
@@ -61,22 +61,19 @@ brew install yarn
 setup_app git
 setup_app zsh
 # ========================================================================= brew
-brew install catimg # Catimg https://github.com/posva/catimg
 brew install bat # bat https://github.com/sharkdp/bat
+brew install catimg # Catimg https://github.com/posva/catimg
 brew install gron # gron https://github.com/tomnomnom/gron
+brew install lsd && setup_app lsd # lsd https://github.com/Peltoche/lsd
 brew install nvm && setup_app nvm # nvm https://github.com/creationix/nvm
 brew install tmux && setup_app tmux # tmux https://tmux.github.io/
-brew install lsd && setup_app lsd # lsd https://github.com/Peltoche/lsd
 # ==================================================================== brew cask
-# brew cask install 5kplayer # 5KPlayer https://www.5kplayer.com/
 brew cask install alacritty # Alacritty https://github.com/jwilm/alacritty
 brew cask install appcleaner # AppCleaner https://freemacsoft.net/appcleaner/
 brew cask install caprine # Caprine https://sindresorhus.com/caprine/
-# brew cask install cheatsheet # CheatSheet https://www.cheatsheetapp.com/CheatSheet/
 brew cask install clipy # Clipy https://clipy-app.com/
 brew cask install docker # Docker https://www.docker.com/
 brew cask install dozer # Dozer https://github.com/Mortennn/Dozer
-# brew cask install fanny # Fanny http://fannywidget.com/
 brew cask install filezilla # FileZilla https://filezilla-project.org/
 brew cask install google-backup-and-sync # Google Backup and Sync https://www.google.com/drive/download/
 brew cask install google-chrome # Google Chrome https://www.google.com/chrome/
@@ -95,10 +92,7 @@ mas install 497799835 # Xcode https://itunes.apple.com/app/id497799835
 mas install 526298438 # Lightshot Screenshot https://itunes.apple.com/app/id526298438
 mas install 539883307 # LINE https://itunes.apple.com/app/id539883307
 mas install 865500966 # feedly https://itunes.apple.com/app/id865500966
-mas install 946399090 # Telegram Desktop https://itunes.apple.com/app/id946399090
-# mas install 1056643111 # Clocker https://itunes.apple.com/app/id1056643111
-# mas install 1085114709 # Parallels Desktop Lite https://itunes.apple.com/app/id1085114709
-# mas install 1088779979 # Mini Calendar https://itunes.apple.com/app/id1088779979
+mas install 946399090 # Telegram Lite https://itunes.apple.com/app/id946399090
 mas install 1127253508 # Dr.Unarchiver https://itunes.apple.com/app/id1127253508
 mas install 1206020918 # Battery Indicator https://itunes.apple.com/app/id1206020918
 mas install 1263070803 # Lungo https://itunes.apple.com/app/id1263070803
