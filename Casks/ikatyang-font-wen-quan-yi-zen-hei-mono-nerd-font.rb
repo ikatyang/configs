c79e5eed2e08ac3a11419e7371dfcbd726ddd809
Ika <ikatyang@gmail.com>
2023-09-01 18:33:37 +0800

    feat(homebrew): add font-wen-quan-yi-zen-hei-mono-nerd-font

---
+++ Casks/ikatyang-font-wen-quan-yi-zen-hei-mono-nerd-font.rb
@@ -0,0 +1,10 @@
+cask "ikatyang-font-wen-quan-yi-zen-hei-mono-nerd-font" do
+  version "1.0.0"
+  sha256 "8ec9433a58ff741b02088c48e387596858383e7528e896bb1908c829ed77ea50"
+
+  url "file://#{File.expand_path('../artifacts/font-wen-quan-yi-zen-hei-mono-nerd-font-1.0.0/WenQuanYiZenHeiMono-Nerd-Font-0xb7-0x7e.ttf', __dir__)}"
+  name "wen-quan-yi-zen-hei-mono-nerd-font"
+  homepage "https://github.com/ikatyang/configs"
+
+  font "WenQuanYiZenHeiMono-Nerd-Font-0xb7-0x7e.ttf"
+end
